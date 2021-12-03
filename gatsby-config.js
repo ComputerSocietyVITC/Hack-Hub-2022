b4fc52d01fdf5af242193eadb70293d196573153
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-12-03 11:38:57 +0530

    Initialization✨

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    // siteUrl: "https://www.yourdomain.tld",
     title: "HackHub2022",
   },
   plugins: [
---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+
+const IndexPage = () => {
+  return (
+    <>
+      <h1 className="text-red-800">
+        Hello World
+      </h1>
+    </>
+  )
+}
+
+export default IndexPage
