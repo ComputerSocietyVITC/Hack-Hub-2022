98c4e7aa92bf55c84adeef17348c352ee0f27c15
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-12-03 12:07:33 +0530

    Addition of PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,26 @@ module.exports = {
     // siteUrl: "https://www.yourdomain.tld",
     title: "HackHub2022",
   },
+
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
