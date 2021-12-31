8531fab223c4689dc134bf2d5a1dcb227e9e0ad4
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-12-31 10:32:48 +0530

    Staging

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <section className = "font-Sansation font-bold text-2xl">
+                Navbar
+            </section>
+        </>
+    )
+}
+
+export default Navbar;
