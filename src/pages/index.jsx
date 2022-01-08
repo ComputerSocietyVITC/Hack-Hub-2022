028fb08376e5788e0ffeccce9b9f903d093418ac
Samank Gupta <samankgupta@gmail.com>
2022-01-08 22:31:27 +0530

    Central Components Added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,16 @@ import CountDown from "../components/countdown";
 import Navbar from "../components/navbar";
 import TimeLine from "../components/timeline";
 import About from "../components/about";
-
+import CentralComponents from "../components/centralComponents";
 
 const IndexPage = () => {
   return (
     <>
 
       <Navbar />
+      <CentralComponents />
       <CountDown />
-      <About /> 
+      <About />
       <TimeLine />
     </>
   );
