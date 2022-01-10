e7382a20424403a4cf41a6574c724c42c47b3a03
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2022-01-10 23:45:28 +0530

    Initial setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <>
+      <section className="font-bold text-red-400">Hello World</section>
+    </>
+  );
+};
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
