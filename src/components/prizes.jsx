5b6d3b6438849f5afd5c9019a6055587020d3461
Srinitish S <87447180+Deceptrax123@users.noreply.github.com>
2022-02-07 14:10:24 +0530

    Added borders  for 1st,2nd and 3rd prize

--- src/components/prizes.jsx
+++ src/components/prizes.jsx
@@ -10,6 +10,7 @@ const prizeList=[
             {
                 id:1,
                 head:"1st Prize",
+                border:"border-yellow-400",
                 items:[
                     {
                         key:1,
@@ -20,6 +21,7 @@ const prizeList=[
             {
                 id:2,
                 head:"2nd Prize",
+                border:"border-gray-400",
                 items:[
                     {
                         key:1,
@@ -29,6 +31,7 @@ const prizeList=[
             { 
                 id:3,
                 head:"3rd Prize",
+                border:"border-red-400",
                 items:[
                     {
                         key:1,
@@ -42,7 +45,7 @@ const prizeList=[
 const Prize=(props)=>{
     const {title}=props;
     return(
-        <section className="text-3xl font-Sansation font-bold uppercase">
+        <section className="text-4xl font-Sansation font-bold uppercase">
             {title}
         </section>
     );
@@ -62,17 +65,17 @@ const Prizes = () => {
                     <div class="h-1 w-full rounded-lg bg-white"></div>
                 </section>
                 {/*<h1 className="text-white font-Sansation font-bold text-4xl text-center">To Be Announced...</h1>*/}
-                {<div>
+                {<div className="lg:mx-40 relative flex space-x-8 justify-between">
                     {prizeList.filter((item)=>{
                         return item.textHeader==="Prizes";
                     })[0].prize.map((item)=>{
                         return(
-                        <div key={item.id}>
+                        <div className={"text-center rounded-3xl border-4 "+item.border} key={item.id}>
                             <Prize title={item.head}/>
-                            <div>
+                            <div className="text-center text-bold leading-loose">
                                 {item.items.map((item)=>{
                                     return(
-                                        <p>To be decided.</p>
+                                        <p>Ex officia qui nulla dolor nisi sint consequat exercitation deserunt. Sunt deserunt qui officia et est. Magna id qui commodo occaecat eiusmod veniam et cupidatat incididunt duis amet do qui cupidatat. Aliqua reprehenderit sit cupidatat ipsum cupidatat. Tempor id aliqua veniam cillum in exercitation elit deserunt nostrud. Velit velit ut amet proident ullamco velit officia nisi esse ad id officia excepteur.</p>
                                     );
                                 })}
                             </div>
