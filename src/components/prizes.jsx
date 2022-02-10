4309e245a865a140391817ee6db2ed3800fbfe51
Srinitish S <87447180+Deceptrax123@users.noreply.github.com>
2022-02-10 14:38:50 +0530

    Prizes Component (#27)
    * Prizes Component
    
    * Added borders  for 1st,2nd and 3rd prize
    
    * Added Participation Prizes
    
    Co-authored-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/prizes.jsx
+++ src/components/prizes.jsx
@@ -63,6 +63,55 @@ const Prize = (props) => {
   );
 };
 
+const prizeList=[
+    {
+        id:1,
+        textHeader:"Prizes",
+        prize:[
+            {
+                id:1,
+                head:"1st Prize",
+                border:"border-yellow-400",
+                items:[
+                    {
+                        key:1,
+
+                    },
+                ]
+            },
+            {
+                id:2,
+                head:"2nd Prize",
+                border:"border-gray-400",
+                items:[
+                    {
+                        key:1,
+                    },
+                ]
+            },
+            { 
+                id:3,
+                head:"3rd Prize",
+                border:"border-red-400",
+                items:[
+                    {
+                        key:1,
+                    },
+                ]
+            }
+        ]
+
+    }
+]
+const Prize=(props)=>{
+    const {title}=props;
+    return(
+        <section className="text-4xl font-Sansation font-bold uppercase">
+            {title}
+        </section>
+    );
+}
+
 const Prizes = () => {
   const prizesRef = useNav("Prizes");
 
@@ -109,28 +158,50 @@ const Prizes = () => {
             })}
         </div>
 
-        <div
-          className="participation mt-20 sm:mx-12 lg:mx-60 rounded-3xl border-2 opacity-90 hover:opacity-100 border-gray-200"
-          id="Participation"
-        >
-          <div className="text-3xl text-center py-3 font-Sansation font-bold uppercase text-white">
-            Participation Prizes
-          </div>
-          <div className="h-0.5 w-full rounded bg-gray-400"></div>
-          <div className="text-bold text-center font-Sansation text-white">
-            <ul className="py-6 px-4 list-disc list-inside leading-loose">
-              <li>100$ Digital Ocean Credits, 60-day Free trial</li>
-              <li>Premium Tier services from Echo3D</li>
-              <li>Free XYZ domain name for 1 year</li>
-              <li>Hacker Plan from Replit</li>
-              <li>15% Discount from Badelog</li>
-              <li>5-Year free upgrades from Taskade</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="mt-24 px-8 sm:px-0 mb-40" ref={prizesRef} id='prizesContainer'>
+                <section className="sm:w-1/3 lg:w-1/5 my-8 py-12">
+                    <section className="text-white font-Sansation font-bold text-5xl text-center">
+                        PRIZES
+                    </section>
+                    <div class="h-1 w-full rounded-lg bg-white"></div>
+                </section>
+                {/*<h1 className="text-white font-Sansation font-bold text-4xl text-center">To Be Announced...</h1>*/}
+                {<div className="lg:mx-40 relative flex space-x-16 justify-between" id="winners">
+                    {prizeList.filter((item)=>{
+                        return item.textHeader==="Prizes";
+                    })[0].prize.map((item)=>{
+                        return(
+                        <div className={"text-center rounded-3xl border-4 "+item.border} key={item.id}>
+                            <Prize title={item.head}/>
+                            <div className="text-center text-bold leading-loose">
+                                {item.items.map((item)=>{
+                                    return(
+                                        <p>Ex officia qui nulla dolor nisi sint consequat exercitation deserunt. Sunt deserunt qui officia et est. Magna id qui commodo occaecat eiusmod veniam et cupidatat incididunt duis amet do qui cupidatat. Aliqua reprehenderit sit cupidatat ipsum cupidatat. Tempor id aliqua veniam cillum in exercitation elit deserunt nostrud. Velit velit ut amet proident ullamco velit officia nisi esse ad id officia excepteur.</p>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        );
+                    })}           
+                </div>}
+                <div className="mt-20 mx-40 rounded-3xl border-4 border-gray-200" id="Participation">
+                    <div className="text-4xl text-center font-Sansation font-bold uppercase">Participation Prizes</div>
+                    <div className="h-1 w-full rounded bg-gray-600"></div>
+                    <div className="text-xl  text-bold text-center font-Sansation">
+                        <ul className="space-y-6">
+                            <li>Premium Resources for free for everyone</li>
+                            <li>Certificates for everyone</li>
+                            <li>15% off to all participants</li>
+                            <li>Free XYZ domain names to everyone</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
 };
 
 export default Prizes;
